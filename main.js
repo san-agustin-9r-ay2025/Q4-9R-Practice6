674f3360bdf181ab03426a610edb3164e608317c
san-agustin-9r-ay2025 <sanagustin.go@obmontessori.edu.ph>
2026-04-24 12:35:14 +0800

    update main

---
+++ main.js
@@ -0,0 +1,7 @@
+function activateCounter() {
+    var animals = ["Lion","Flamingo","Polar Bear","Boa Constrictor"];
+
+    for (var i = 0; i < animals.length; i++) {
+        window.alert("This zoo contains a " + animals[i] + ".");
+    }
+}
